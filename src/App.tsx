8b2ac3eb5eeca810944e49f58d9ceef8148d65ee
Juan Aguilar <juan-carlos.aguilar@atos.net>
2022-03-25 12:37:25 -0500

    Project set up

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,14 @@ import { NavBar } from "./components/navBar";
 import { AllProducts } from "./pages/allProducts";
 
 class App extends Component {
+  state = {
+    products: [
+      { name: 'Tomato', price: 1500, img: '' },
+      { name: 'Green Beans', price: 1200, img: '' },
+      { name: 'Lettuce', price: 1000, img: '' }
+    ]
+  }
+  
   render() {
     return (
       <div className="App">
