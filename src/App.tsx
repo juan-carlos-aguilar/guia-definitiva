b7338fd870d5adc3dabb922a4c8fdd6d82760178
Juan Aguilar <juan-carlos.aguilar@atos.net>
2022-03-25 15:23:40 -0500

    Funciones puras

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import { Component } from "react";
 import './App.css';
 
+// Funcion pura
+const MyComp = ({ prop }) => {
+  return (
+    <div>
+      Name: {prop}
+    </div>
+  )
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Branch for Hook practices</h1>
+        <MyComp prop={'Carlos'}/>
       </div>
     )
   }
