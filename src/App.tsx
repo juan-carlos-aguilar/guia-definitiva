f74d77fb412d393aa4d8f110c1eeec1bfd059556
Juan Aguilar <juan-carlos.aguilar@atos.net>
2022-03-25 12:24:56 -0500

    Deleted files for main branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import { Component } from "react";
 import './App.css';
-import { NavBar } from "./components/navBar";
-import { AllProducts } from "./pages/allProducts";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <NavBar />
-        <AllProducts />
+        <h1>Initial Commit</h1>
       </div>
     )
   }
