0e7a5bd1aa77c2006eabb45736770fd77e652dff
Juan Aguilar <juan-carlos.aguilar@atos.net>
2022-03-28 16:52:51 -0500

    Functionality done

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,61 @@
-import { Component } from "react";
+import { Component, useState } from "react";
 import './App.css';
+import useFormulario from './hooks/useFormulario'
+import Input from "./components/Input/Input";
+import Card from "./components/Card/Card";
+import { Container } from "./components/Container/Container";
+import Button from "./components/Button/Button";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Gestor de Usuarios</h1>
-      </div>
-    )
+function App() {
+  const [usuarios, setUsuarios] = useState([])
+  const [formulario, handleChange] = useFormulario({
+    name: '',
+    lastname: '',
+    email: ''
+  })
+
+  const submit = e => {
+    e.preventDefault()
+    setUsuarios([
+      ...usuarios,
+      formulario,
+    ])
   }
+
+  return (
+    <Container>
+      <Card>
+        <form onSubmit={submit}>
+          <Input
+            label="Nombre"
+            name="name"
+            value={formulario.name}
+            onChange={handleChange}
+          />
+          <Input
+            label="Apellido"
+            name="lastname"
+            value={formulario.lastname}
+            onChange={handleChange}
+          />
+          <Input
+            label="Correo"
+            name="email"
+            value={formulario.email}
+            onChange={handleChange}
+          />
+          <Button>Enviar</Button>
+        </form>
+      </Card>
+      <Card>
+        <ul>
+          {usuarios.map(x =>
+            <li key={x.email}>{`${x.name} ${x.lastname}: ${x.email}`}</li>
+          )}
+        </ul>
+      </Card>
+    </Container>
+  )
 }
 
 export default App
