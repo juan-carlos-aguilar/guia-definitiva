1c0f3391d740e2c287c54137fa85b4eaf32479ae
Juan Aguilar <juan-carlos.aguilar@atos.net>
2022-03-29 10:14:05 -0500

    Pseudo elements & selectores

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,26 @@ const P = styled.p`
 `
 
 const Button = styled.button`
+  transition: box-shadow 0.2s ease;
   width: 100px;
   height: 50px;
   background-color: ${props => props.primary ? 'red' : 'white'};
   color: ${props => props.primary ? 'white' : 'red'};
   border: solid 2px red;
   border-radius: 4px;
+  margin: 10px;
+
+  &:hover {
+    box-shadow: 1px 2px 4px rgb(0,0,0,0.3);
+  } 
+`
+
+const Link = ({ className, ...props }) => {
+  return <a className={className} {...props}></a>
+}
+
+const StyledLink = styled(Link)`
+  color: blue;
 `
 
 function App() {
@@ -23,7 +37,9 @@ function App() {
       <h1>Styled Components</h1>
       <P>Carlos</P>
       <Button>Enviar</Button>
-      <Button primary>Enviar</Button>
+      <Button primary>Enviar</Button><br/>
+      <Link>Link</Link><br />
+      <StyledLink>Styled Link</StyledLink>
     </div>
   )
 }
