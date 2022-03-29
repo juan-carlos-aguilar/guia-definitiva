727cfca40145cfb08777084d38281a812183e475
Juan Aguilar <juan-carlos.aguilar@atos.net>
2022-03-29 10:48:23 -0500

    Project set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { Component } from "react";
 import './App.css';
 
-class App extends Component {
-  render() {
+function App() {
     return (
       <div className="App">
         <h1>Initial Commit</h1>
       </div>
     )
-  }
 }
 
 export default App
