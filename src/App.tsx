c61e397ec55f24f72d92260de8dabdb5c0868d97
Juan Aguilar <juan-carlos.aguilar@atos.net>
2022-03-29 12:24:40 -0500

    Added form and some functionality
    - Added form fields
    - Style
    - Error Handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,50 @@
 //import './App.css';
 import styled from 'styled-components';
+import { Formik, Form } from 'formik';
+import Input from './components/Input'
 
+// Adding styles with Styled-components
 const Container = styled.div`
   display: flex;
   justify-content: center;
   height: 100%;
   align-items: center;
 `
+const Section = styled.section`
+  background-color: #eee;
+  border-top: solid 2px palevioletred;
+  padding: 20px 25px;
+  width: 500px;
+  box-shadow: 0px 2px 3px rgb(0,0,0,0.3);
+`
 
 function App() {
-    return (
-      <Container>
-        Carlos
-      </Container>
-    )
+  const handleSubmit = () => {
+
+  }
+
+  return (
+    <Container>
+      <Section>
+        <Formik
+          initialValues={{
+            deposit: '',
+            contribution: '',
+            years: '',
+            rate: '',
+          }}
+          onSubmit={handleSubmit}
+        >
+          <Form>
+            <Input name="deposit" label="Deposito Inicial" />
+            <Input name="contribution" label="Contribucion Anual" />
+            <Input name="years" label="Años" />
+            <Input name="rate" label="Interes estimado" />
+          </Form>
+        </Formik>
+      </Section>
+    </Container>
+  )
 }
 
 export default App
