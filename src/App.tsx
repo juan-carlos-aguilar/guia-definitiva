1a7c93ae76a0ab8a2c6ce5be836b88cfa7e1288c
Juan Aguilar <juan-carlos.aguilar@atos.net>
2022-03-29 10:01:48 -0500

    Props and components

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,24 @@ import './App.css';
 const P = styled.p`
   font-size: 24px;
   color: red;
-` 
+`
+
+const Button = styled.button`
+  width: 100px;
+  height: 50px;
+  background-color: ${props => props.primary ? 'red' : 'white'};
+  color: ${props => props.primary ? 'white' : 'red'};
+  border: solid 2px red;
+  border-radius: 4px;
+`
 
 function App() {
   return (
     <div className="App">
       <h1>Styled Components</h1>
       <P>Carlos</P>
+      <Button>Enviar</Button>
+      <Button primary>Enviar</Button>
     </div>
   )
 }
