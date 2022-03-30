ef6457c8e756ad4c69d28b766563ad4e16bf955a
Juan Aguilar <juan-carlos.aguilar@atos.net>
2022-03-30 16:25:09 -0500

    Added loading reducer/action

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,64 @@
-import { useState } from "react";
-import { combineReducers } from "redux";
-import { useSelector, useDispatch } from "react-redux";
+import { useState } from 'react';
+import { combineReducers } from 'redux';
+import { useSelector, useDispatch } from 'react-redux';
+import './App.css'
 
-export const asyncMiddleware = store => next => action => {
+export const asyncMiddleware = (store) => (next) => (action) => {
   if (typeof action === 'function') {
-    return action(store.dispatch, store.getState)
+    return action(store.dispatch, store.getState);
   }
-  return next(action)
-}
+  return next(action);
+};
 
-export const fetchThunk = () => async dispatch => {
-  dispatch({ type: 'todos/pending' })
+export const fetchThunk = () => async (dispatch) => {
+  dispatch({ type: 'todos/pending' });
   try {
-    const response = await fetch('https://jsonplaceholder.typicode.com/todos')
-    const data = await response.json()
-    const todos = data.slice(0, 10)
-    dispatch({ type: 'todos/fulfilled', payload: todos })
-  } catch(e) {
-    dispatch({ type: 'todos/error', error: e.message })
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+    const data = await response.json();
+    const todos = data.slice(0, 10);
+    dispatch({ type: 'todos/fulfilled', payload: todos });
+  } catch (e) {
+    dispatch({ type: 'todos/error', error: e.message });
   }
-}
+};
 
-export const filterReducer = (state = "all", action) => {
+export const filterReducer = (state = 'all', action) => {
   switch (action.type) {
-    case "filter/set":
+    case 'filter/set':
       return action.payload;
     default:
       return state;
   }
 };
 
+const initialFetching = { loading: 'idle', error: null };
+export const fetchingReducer = (state = initialFetching, action) => {
+  switch (action.type) {
+    case 'todos/pending': {
+      return { ...state, loading: 'pending' };
+    }
+    case 'todos/fulfilled': {
+      return { ...state, loading: 'succeded' };
+    }
+    case 'todos/error': {
+      return { error: action.error, loading: 'rejected' }
+    }
+    default: {
+      return state
+    }
+  }
+};
+
 export const todosReducer = (state = [], action) => {
   switch (action.type) {
     case 'todos/fulfilled': {
-      return action.payload
+      return action.payload;
     }
-    case "todo/add": {
+    case 'todo/add': {
       return state.concat({ ...action.payload });
     }
-    case "todo/complete": {
-      const newTodos = state.map(todo => {
+    case 'todo/complete': {
+      const newTodos = state.map((todo) => {
         if (todo.id === action.payload.id) {
           return { ...todo, completed: !todo.completed };
         }
@@ -54,39 +73,48 @@ export const todosReducer = (state = [], action) => {
 };
 
 export const reducer = combineReducers({
-  entities: todosReducer,
+  todos: combineReducers({
+    entities: todosReducer,
+    status: fetchingReducer,
+  }),
   filter: filterReducer,
-})
+});
+
+
+const selectTodos = (state) => {
+  const { todos: { entities }, filter } = state;
+  if (filter === 'complete') {
+    return entities.filter((todo) => todo.completed);
+  }
+  
+  if (filter === 'incomplete') {
+    return entities.filter((todo) => !todo.completed);
+  }
+  
+  return entities;
+};
+
+const selectStatus = state => state.todos.status
 
 const TodoItem = ({ todo }) => {
   const dispatch = useDispatch();
 
   return (
     <li
-      style={{ textDecoration: todo.completed ? "line-through" : "none" }}
-      onClick={() => dispatch({ type: "todo/complete", payload: todo })}
+      style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}
+      onClick={() => dispatch({ type: 'todo/complete', payload: todo })}
     >
       {todo.title}
     </li>
   );
 };
 
-const selectTodos = (state) => {
-  const { entities, filter } = state;
-  if (filter === "complete") {
-    return entities.filter((todo) => todo.completed);
-  }
-
-  if (filter === "incomplete") {
-    return entities.filter((todo) => !todo.completed);
-  }
-
-  return entities;
-};
 const App = () => {
-  const [value, setValue] = useState("");
+  const [value, setValue] = useState('');
   const dispatch = useDispatch();
+  
   const todos = useSelector(selectTodos);
+  const status = useSelector(selectStatus);
 
   // handleform submit
   const submit = (e) => {
@@ -96,25 +124,33 @@ const App = () => {
     }
     const id = Math.random().toString(36);
     const todo = { title: value, completed: false, id };
-    dispatch({ type: "todo/add", payload: todo });
-    setValue("");
+    dispatch({ type: 'todo/add', payload: todo });
+    setValue('');
   };
 
+  if (status.loading === 'pending') {
+    return <p>Cargando...</p>
+  }
+
+  if (status.loading === 'rejected') {
+    return <p>{status.error}</p>
+  }
+
   return (
-    <div>
+    <div className='App'>
       <form onSubmit={submit}>
         <input value={value} onChange={(e) => setValue(e.target.value)} />
       </form>
-      <button onClick={() => dispatch({ type: "filter/set", payload: "all" })}>
+      <button onClick={() => dispatch({ type: 'filter/set', payload: 'all' })}>
         All Items
       </button>
       <button
-        onClick={() => dispatch({ type: "filter/set", payload: "complete" })}
+        onClick={() => dispatch({ type: 'filter/set', payload: 'complete' })}
       >
         Complete
       </button>
       <button
-        onClick={() => dispatch({ type: "filter/set", payload: "incomplete" })}
+        onClick={() => dispatch({ type: 'filter/set', payload: 'incomplete' })}
       >
         Incomplete
       </button>
