be9dab8c66a0ab93fcf303d2d4b2710adaf75859
Juan Aguilar <juan-carlos.aguilar@atos.net>
2022-03-30 14:41:27 -0500

    Refactoring reducers

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,145 @@
-import { useSelector, useDispatch } from "react-redux"
+import { useDebugValue, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 const initialState = {
   entities: [],
-}
+  filter: "all",
+};
 
-export const reducer = (state = initialState, action ) => {
-  switch(action.type) {
-    case 'todo/add': {
-      return {
-        ...state,
-        entities: [{}]
-      }
-    }
+export const filterReducer = (state = "all", action) => {
+  switch (action.type) {
+    case "filter/set":
+      return action.payload;
+    default:
+      return state;
   }
-  return state
-}
+};
 
-const App = () => {
+export const todosReducer = (state = [], action) => {
+  switch (action.type) {
+    case "todo/add": {
+      return state.concat({ ...action.payload });
+    }
+    case "todo/complete": {
+      const newTodos = state.map(todo => {
+        if (todo.id === action.payload.id) {
+          return { ...todo, completed: !todo.completed };
+        }
+
+        return todo;
+      });
+      return newTodos;
+    }
+    default:
+      return state;
+  }
+};
+
+export const reducer = (state = initialState, action) => {
+  return {
+    entities: todosReducer(state.entities, action),
+    filter: filterReducer(state.filter, action),
+  };
+};
+
+// Reducer, handles complete/incomplete states
+// export const reducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case 'todo/add': {
+//       return {
+//         ...state,
+//         entities: state.entities.concat({ ...action.payload })
+//       }
+//     }
+//     case 'todo/complete': {
+//       const newTodos = state.entities.map(todo => {
+//         if (todo.id === action.payload.id) {
+//           return { ...todo, completed: !todo.completed }
+//         }
+//         return todo
+//       })
+//       return {
+//         ...state,
+//         entities: newTodos
+//       }
+//     }
+//     case 'filter/set': {
+//       return {
+//         ...state,
+//         filter: action.payload,
+//       }
+//     }
+//   }
+//   return state
+// }
+
+const TodoItem = ({ todo }) => {
   const dispatch = useDispatch();
-  const state = useSelector(x => x);
-  console.log(state, 'rendering');
+
+  return (
+    <li
+      style={{ textDecoration: todo.completed ? "line-through" : "none" }}
+      onClick={() => dispatch({ type: "todo/complete", payload: todo })}
+    >
+      {todo.title}
+    </li>
+  );
+};
+
+const selectTodos = (state) => {
+  const { entities, filter } = state;
+  if (filter === "complete") {
+    return entities.filter((todo) => todo.completed);
+  }
+
+  if (filter === "incomplete") {
+    return entities.filter((todo) => !todo.completed);
+  }
+
+  return entities;
+};
+const App = () => {
+  const [value, setValue] = useState("");
+  const dispatch = useDispatch();
+  const todos = useSelector(selectTodos);
+
+  // handleform submit
+  const submit = (e) => {
+    e.preventDefault();
+    if (!value.trim()) {
+      return;
+    }
+    const id = Math.random().toString(36);
+    const todo = { title: value, completed: false, id };
+    dispatch({ type: "todo/add", payload: todo });
+    setValue("");
+  };
+
   return (
     <div>
-      <form action="">
-        <input />
+      <form onSubmit={submit}>
+        <input value={value} onChange={(e) => setValue(e.target.value)} />
       </form>
-      <button onClick={() => dispatch({ type: 'todo/add'})}>Todos</button>
-      <button>Completados</button>
-      <button>Incompletados</button>
+      <button onClick={() => dispatch({ type: "filter/set", payload: "all" })}>
+        All Items
+      </button>
+      <button
+        onClick={() => dispatch({ type: "filter/set", payload: "complete" })}
+      >
+        Complete
+      </button>
+      <button
+        onClick={() => dispatch({ type: "filter/set", payload: "incomplete" })}
+      >
+        Incomplete
+      </button>
       <ul>
-        <li>todo 1</li>
-        <li>todo 2</li>
+        {todos.map((todo) => (
+          <TodoItem key={todo.id} todo={todo} />
+        ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
