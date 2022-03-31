a1bbb25ba6947e1bd72727d8821576d000aab7c1
Juan Aguilar <juan-carlos.aguilar@atos.net>
2022-03-31 18:02:03 -0500

    Pokeapp Done

--- next/next.config.js
+++ next/next.config.js
@@ -1,6 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ['raw.githubusercontent.com']
+  }
 }
-
-module.exports = nextConfig
